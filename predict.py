fe5a1d85fdbf28c0e7ece8d5dccce2b8bdde979e
Men1scus <zzh_cd@126.com>
2024-07-28 21:38:09 +0800

    [FEAT] BiRefNet-general-epoch_244.pth

--- predict.py
+++ predict.py
@@ -18,6 +18,7 @@ device = config.device
 class ImagePreprocessor():
     def __init__(self, resolution=(1024, 1024)) -> None:
         self.transform_image = transforms.Compose([
+            # transforms.Resize(resolution),    # 1. keep consistent with the cv2.resize used in training 2. redundant with that in path_to_image()
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
         ])
@@ -36,6 +37,7 @@ def load_model(state_dict_path):
     if os.path.exists(state_dict_path):
         birefnet_dict = torch.load(state_dict_path, map_location="cpu")
         unwanted_prefix = '_orig_mod.'
+        # 移除状态字典中不需要的前缀
         for k, v in list(birefnet_dict.items()):
             if k.startswith(unwanted_prefix):
                 birefnet_dict[k[len(unwanted_prefix):]] = birefnet_dict.pop(k)
@@ -44,54 +46,68 @@ def load_model(state_dict_path):
     model.eval()
     return model
 
-def predict_single_image(model, image_path, resolution=(1024, 1024)):
-    image = cv2.imread(image_path)
+def predict(model, image, resolution):
+
+    image = cv2.imread(image)
     if image is None:
         raise ValueError("Image not found or unable to read.")
     
-    original_shape = image.shape[:2]
-    image_pil = array_to_pil_image(image, resolution)
+    # 把 OpenCV 读取的 BGR 格式图片 转换为 Pytorch 中的 RGB
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    
+    # 用原图的分辨率
+    resolution = f"{image.shape[1]}x{image.shape[0]}" if resolution == '' else resolution
+    resolution = [int(int(reso)//32*32) for reso in resolution.strip().split('x')]
+
+    images = [image_rgb]
+    image_shapes = [image.shape[:2] for image in images]
+    images = [array_to_pil_image(image, resolution) for image in images]
 
     image_preprocessor = ImagePreprocessor(resolution=resolution)
-    image_proc = image_preprocessor.proc(image_pil).unsqueeze(0).to(device)
+    images_proc = []
+    for image in images:
+        images_proc.append(image_preprocessor.proc(image))
+    images_proc = torch.cat([image_proc.unsqueeze(0) for image_proc in images_proc])
 
     with torch.no_grad():
-        scaled_pred_tensor = model(image_proc)[-1].sigmoid()
-    pred = torch.nn.functional.interpolate(
-        scaled_pred_tensor, size=original_shape, mode='bilinear', align_corners=True
-    ).squeeze().cpu().numpy()
+        scaled_preds_tensor = model(images_proc.to(device))[-1].sigmoid()
+        
+    preds = []
+    for image_shape, pred_tensor in zip(image_shapes, scaled_preds_tensor):
+        if device == 'cuda':
+            pred_tensor = pred_tensor.cpu()
+        preds.append(torch.nn.functional.interpolate(pred_tensor.unsqueeze(0), size=image_shape, mode='bilinear', align_corners=True).squeeze().numpy())
 
-    pred_image = np.repeat(np.expand_dims(pred, axis=-1), 3, axis=-1) * 255
-    pred_image = pred_image.astype(np.uint8)
+    image_preds = []
+    for image, pred in zip(images, preds):
+        image = image.resize(pred.shape[::-1])
+        pred = (pred * 255).astype(np.uint8)  # Scale prediction to 0-255
 
-    return pred_image
+        # Create RGBA image
+        rgba_image = np.dstack((np.array(image), pred))
+        rgba_image = Image.fromarray(rgba_image, 'RGBA')
+        image_preds.append(rgba_image)
 
-def create_transparent_background_image(image, pred_image):
-    alpha_channel = (pred_image[:, :, 0] > 127).astype(np.uint8) * 255
-    rgba_image = np.dstack((image, alpha_channel))
-    return rgba_image
+    return image_preds[0]
 
 def save_image(image, save_path):
-    cv2.imwrite(save_path, image)
+    image.save(save_path)
 
 class Predictor(BasePredictor):
     def setup(self) -> None:
         """Load the model into memory to make running multiple predictions efficient"""
-        self.model = load_model("BiRefNet-massive-epoch_240.pth")
+        self.model = load_model("BiRefNet-general-epoch_244.pth")
 
     def predict(
         self,
         image: Path = Input(description="Input image"),
         resolution: str = Input(
             description="Resolution in WxH format, e.g., '1024x1024'",
-            default="1024x1024"
+            default=""
         ),
     ) -> Path:
         """Run a single prediction on the model"""
-        resolution = tuple(map(int, resolution.split('x')))
-        original_image = cv2.imread(str(image))
-        output_image = predict_single_image(self.model, str(image), resolution)
-        transparent_image = create_transparent_background_image(original_image, output_image)
+        output_image = predict(self.model, str(image), resolution)
         output_path = "output.png"
-        save_image(transparent_image, output_path)
+        save_image(output_image, output_path)
         return Path(output_path)
